477d086cc903abf0f21fe17043bfcd03d9a184d7
Josh Adams <josh@isotope11.com>
2011-03-08 19:27:27 -0600

    base rails app to support the service, functionality to come

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitpusshutenDeployButton::Application.config.secret_token = '402e9f376aaf4f3e77b0e0cc66e69ff7154c18321bca6ede395a707b5287024ae21933f4b07822edbcf6c168e637f7c8c4ab1df37cb80048090d9bbf23231f7b'
